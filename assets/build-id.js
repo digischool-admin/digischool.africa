9dc3a0ec96f1f502255c50a573f2348eca82deda
digischool-admin <genspark_dev@genspark.ai>
2026-02-01 12:29:18 +0000

    UPDATE BUILD_ID: 1890a67 with cache-buster

--- assets/build-id.js
+++ assets/build-id.js
@@ -1,4 +1,4 @@
 // DigiSchool Africa - Build ID
 // Auto-generated from git commit hash
-// BUILD_TS=1738343628
-window.DS_BUILD_ID = "9def839";
+// BUILD_TS=1738343889
+window.DS_BUILD_ID = "1890a67";
